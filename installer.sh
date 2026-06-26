f92abad3e7c314b6dc557a00411e5cd9fa30f170
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-06-26 20:29:21 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -177,16 +177,5 @@ IMAGE NAME: ${distro_value:-Unknown}
 IMAGE VERSION: ${distro_version:-Unknown}
 EOF
 
-echo "Restarting enigma2 in 3 seconds..."
-sleep 3
-
-# Restart Enigma2
-if command -v systemctl >/dev/null 2>&1; then
-    systemctl restart enigma2
-elif command -v init >/dev/null 2>&1; then
-    init 4 && sleep 2 && init 3
-else
-    killall -9 enigma2
-fi
 
 exit 0
